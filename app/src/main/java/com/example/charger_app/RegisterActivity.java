ee1c4bd3bc4ec055aeb95f8e79ef27138a7fe003
M10D12 <107938456+M10D12@users.noreply.github.com>
2024-07-19 17:24:29 +0100

    update

--- app/src/main/java/com/example/charger_app/RegisterActivity.java
+++ app/src/main/java/com/example/charger_app/RegisterActivity.java
@@ -1,4 +1,4 @@
-// LoginActivity.java
+// RegisterActivity.java
 
 package com.example.charger_app;
 
@@ -8,20 +8,20 @@ import android.view.View;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
         // Encontrar o TextView e configurar o OnClickListener
-        TextView signUpTextView = findViewById(R.id.tvSignUp);
-        signUpTextView.setOnClickListener(new View.OnClickListener() {
+        TextView loginTextView = findViewById(R.id.tvLogin);
+        loginTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Iniciar a RegisterActivity
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                // Iniciar a LoginActivity
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
